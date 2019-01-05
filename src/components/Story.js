bbc5a6966537bcb84bb0e2dcfd1276b756fd9390
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2019-01-05 18:18:09 +0200

    adding column header in Stories.js

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./Story.css";
 
-const Story = ({ story }) => {
+const Story = ({ story, columns }) => {
   const { title, url, author, num_comments, points } = story;
 
   return (
     <div className="story">
-      <span>
+      <span style={{ width: columns.title.width }}>
         <a href={url}>{title}</a>
       </span>
-      <span>{author}</span>
-      <span>{num_comments}</span>
-      <span>{points}</span>
+      <span style={{ width: columns.author.width }}>{author}</span>
+      <span style={{ width: columns.comments.width }}>{num_comments}</span>
+      <span style={{ width: columns.points.width }}>{points}</span>
+      <span style={{ width: columns.archive.width }} />
     </div>
   );
 };
