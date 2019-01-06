81ad3c5401689eca36a89d755bcff2393e2a1483
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2019-01-06 21:15:38 +0200

    redux: selector layer

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import store from "./store";
 import { STORY_ARCHIVE } from "./constants/actionTypes";
+import { getReadableStories } from "./selectors/story";
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <App
-    stories={store.getState().storyState}
+    //stories={store.getState().storyState}
+    stories={getReadableStories(store.getState())}
     onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
   />,
   document.getElementById("root")
 );
-registerServiceWorker();
