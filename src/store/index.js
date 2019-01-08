10398ce37a460dd0d5049abe1a56a40594c58760
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2019-01-08 13:46:05 +0200

    installing redux-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
-import { createStore } from "redux";
-import rootReducer from "../reducers";
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../reducers';
+import rootSaga from '../sagas';
+
+const logger = createLogger();
+const saga = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  undefined,
+  applyMiddleware(saga, logger)
 );
+
+saga.run(rootSaga);
 
 export default store;
